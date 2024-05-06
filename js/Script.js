5761a4dab0c3c7f1aef51cfb5eecc24336a70230
GalalElaasar9 <138381852+GalalElaasar9@users.noreply.github.com>
2024-05-06 13:04:48 +0300

    Initial commit

---
+++ js/Script.js
@@ -0,0 +1,51 @@
+lightGallery(document.querySelector(".Gallery .gallery-container"));
+
+$(document).ready(function(){
+    $('button.top').click(function(){
+        $("html , body").animate({
+            scrollTop:0,  
+    },1000); 
+});
+
+$(window).scroll(function(){
+  if($(this).scrollTop() > 300){
+        $('button.top').fadeIn();  
+  }else{
+        $('button.top').fadeOut();  
+  }  
+}); 
+});
+
+
+
+
+let scrolling = $(window).scrollTop();
+
+  $(window).scroll(function () {
+    if (scrolling < $(window).scrollTop()) {
+      $(".navbar").slideUp();
+    } else {
+      $(".navbar").slideDown();
+    }
+    scrolling = $(window).scrollTop();
+  });
+
+
+
+function counterUp(element , limit , speed){
+  let counter = 0;
+
+  let x = setInterval(function(){
+    counter++
+    $(element).html(counter)
+    if(counter == limit){
+      clearInterval(x)
+    }
+  },speed);
+}
+function startCounter(){
+  counterUp("#repairsDone" , 200 , 1000)
+  counterUp("#awardsWon" , 25 , 1000)
+  counterUp("#happyClients" , 250 , 1000)
+  counterUp("#activeWorkers" , 45 , 1000)
+}
